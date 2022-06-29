308d8f4cdb9fe6e57b343ec6e2164dfe9dea2547
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-29 06:44:45 +0200

    has model

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { sequelize } = require('./models');
 
 const app = express();
 const port = 3022;
@@ -7,6 +8,7 @@ app.get('/', (req, res) => {
     res.send('test api');
 });
 
-app.listen(port, () => {
+app.listen(port, async () => {
     console.log(`Listening on http://localhost:${port}`);
+	await sequelize.sync();
 });
